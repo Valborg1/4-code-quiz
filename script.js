fa7a456dc3dc1a496914d87c7cb0a3a890e37793
Jared Lim <jaredlim1@gmail.com>
2020-10-08 16:13:33 -0600

    updates to timer and questions array

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+
+
+
+
+
+
+
+// Countdown Timer
 function countdown( elementName, minutes, seconds )
 {
     var element, endTime, hours, mins, msLeft, time;
@@ -15,47 +23,117 @@ function countdown( elementName, minutes, seconds )
         } else {
             time = new Date( msLeft );
             hours = time.getUTCHours();
-            mins = time.getUTCMinutes();
-            element.innerHTML = (hours ? hours + ':' + twoDigits( mins ) : mins) + ':' + twoDigits( time.getUTCSeconds() );
+            $mins = time.getUTCMinutes();
+            element.innerHTML = (hours ? hours + ':' + twoDigits( $mins ) : $mins) + ':' + twoDigits( time.getUTCSeconds() );
             setTimeout( updateTimer, time.getUTCMilliseconds() + 500 );
         }
+        // console.log($("#ten-countdown").text());
+        $time = Math.floor(($("#ten-countdown").text()));
+        $time = Number($time);
+        console.log(mins);
     }
 
     element = document.getElementById( elementName );
     endTime = (+new Date) + 1000 * (60*minutes + seconds) + 500;
     updateTimer();
+
+
 }
 
 countdown( "ten-countdown", 10, 0 );
 
 
-$(function(){
-    var loading = $('#loadbar').hide();
-    $(document)
-    .ajaxStart(function () {
-        loading.show();
-    }).ajaxStop(function () {
-    	loading.hide();
-    });
+// QUESTIONS SECTION
+
+// Set the correct number for the current question
+$("#q-num").text(questionNumber+1);
+
+// Set the Question and Answer Options
+$("#q-text").text(questions[questionNumber].question);
+$(".a1").text(questions[questionNumber].answer1);
+$(".a2").text(questions[questionNumber].answer2);
+$(".a3").text(questions[questionNumber].answer3);
+$(".a4").text(questions[questionNumber].answer4);
+
+// Answer Click Function
+$("label.btn").on('click',function (evt) {
+    evt.preventDefault();
+    var choice = $(this).find('input:radio').val();
+    $ans = questions[questionNumber].correct;
+
+    check(choice);
+    questionNumber++;
+
+    // Set the correct number for the current question
+    $("#q-num").text(questionNumber+1);
     
-    $("label.btn").on('click',function () {
-    	var choice = $(this).find('input:radio').val();
-    	$('#loadbar').show();
-    	$('#quiz').fadeOut();
-    	setTimeout(function(){
-           $( "#answer" ).html(  $(this).checking(choice) );      
-            $('#quiz').show();
-            $('#loadbar').fadeOut();
-           /* something else */
-    	}, 1500);
-    });
+    // Set the Question and Answer Options
+    $("#q-text").text(questions[questionNumber].question);
+    $(".a1").text(questions[questionNumber].answer1);
+    $(".a2").text(questions[questionNumber].answer2);
+    $(".a3").text(questions[questionNumber].answer3);
+    $(".a4").text(questions[questionNumber].answer4);
 
-    $ans = 3;
+});
 
-    $.fn.checking = function(ck) {
-        if (ck != $ans)
-            return 'INCORRECT';
-        else 
-            return 'CORRECT';
-    }; 
-});	
+currentScore = 0;
+
+function check(a) {
+    if (a != $ans) {
+        return $(".rightWrong").text('INCORRECT');
+      } else { 
+        currentScore+ (1* $time);
+        return $(".rightWrong").text('CORRECT'); 
+      }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $("label.btn").on('click',function () {
+//     var choice = $(this).find('input:radio').val();
+//     $ans = questions[questionNumber].correct;
+//         setTimeout(function(){
+//             $("#rightWrong").html($(this).checking(choice));     
+//             }, 500);
+    
+//     // console.log("Before adding 1: " + questionNumber);
+//     // questionNumber++;
+//     // console.log("After adding 1: " + questionNumber);
+    
+//     function check(choice) {
+//     if (choice != $ans)
+//         return $(".rightWrong").text('INCORRECT');
+//     else 
+//         return $(".rightWrong").text('CORRECT'); 
+//     }
+
+//     $.fn.checking = function(ck) { 
+//         if (ck != $ans)
+//             return $(".rightWrong").text('INCORRECT');
+//         else 
+//             return $(".rightWrong").text('CORRECT');
+//     }; 
+
+
+//     // Set the correct number for the current question
+//     $("#q-num").text(questionNumber+1);
+    
+//     // Set the Question and Answer Options
+//     $("#q-text").text(questions[questionNumber].question);
+//     $(".a1").text(questions[questionNumber].answer1);
+//     $(".a2").text(questions[questionNumber].answer2);
+//     $(".a3").text(questions[questionNumber].answer3);
+//     $(".a4").text(questions[questionNumber].answer4);
+// });
+
