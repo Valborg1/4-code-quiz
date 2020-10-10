0b2f24438ad8c53fbfd3a499710957af902321ea
Jared Lim <jaredlim1@gmail.com>
2020-10-09 20:07:59 -0600

    added a score board and fixed timer issues

--- script.js
+++ script.js
@@ -2,46 +2,70 @@
 
 
 
+// Set Best Score at Game Start
+var scoreArr = JSON.parse(localStorage.getItem("score")) || [];
+$(".bestScore").text(": " + scoreArr[0].score);
+
+
 
 
 
 // Countdown Timer
-function countdown( elementName, minutes, seconds )
-{
-    var element, endTime, hours, mins, msLeft, time;
 
-    function twoDigits( n )
-    {
-        return (n <= 9 ? "0" + n : n);
+var time = 60;
+var timerEl = $("#ten-countdown");
+var timer;
+
+
+function countdown() {
+    time--;
+    timerEl.text(time);
+    if (time <= 0) {
+        gameOver();
     }
+}
+timerEl.text(time);
+timer = setInterval(countdown,1000);
 
-    function updateTimer()
-    {
-        msLeft = endTime - (+new Date);
-        if ( msLeft < 1000 ) {
-            element.innerHTML = "Time is up!";
-        } else {
-            time = new Date( msLeft );
-            hours = time.getUTCHours();
-            $mins = time.getUTCMinutes();
-            element.innerHTML = (hours ? hours + ':' + twoDigits( $mins ) : $mins) + ':' + twoDigits( time.getUTCSeconds() );
-            setTimeout( updateTimer, time.getUTCMilliseconds() + 500 );
-        }
-        // console.log($("#ten-countdown").text());
-        $time = Math.floor(($("#ten-countdown").text()));
-        $time = Number($time);
-        console.log(mins);
-    }
-
-    element = document.getElementById( elementName );
-    endTime = (+new Date) + 1000 * (60*minutes + seconds) + 500;
-    updateTimer();
-
-
+function gameOver() {
+    setTimeout(function () {
+    clearInterval(timer);
+    var score = timerEl.text();
+    timerEl.text("Game Over!");
+    $("#question-display").css("display", "none");
+    $("#game-over").css("display","block");
+    $("#final-score").text(score);
+    $(".rightWrong").text("");
+    }, 2000);
 }
 
-countdown( "ten-countdown", 10, 0 );
+$("#submit-score").on("click", function(){
+    $("#score-card").css("display","block");
+    var submitInitials = $("#initials").val().trim();
+    var submitScore = $("#final-score").text();
 
+    var newScore = {
+        score : submitScore,
+        initials : submitInitials
+    };
+
+    var scoreArr = JSON.parse(localStorage.getItem("score")) || [];
+    scoreArr.push(newScore);
+    
+    // Sort Array
+    scoreArr.sort(function(a,b){
+        return b.score - a.score;
+    })
+
+    for (var i = 0; i < scoreArr.length;i++) {
+        var userScore = $("<li>").text(scoreArr[i].initials + ": " + scoreArr[i].score);
+        $("#all-scores").append(userScore);
+    }
+
+
+    localStorage.setItem("score", JSON.stringify(scoreArr));
+
+})
 
 // QUESTIONS SECTION
 
@@ -62,78 +86,39 @@ $("label.btn").on('click',function (evt) {
     $ans = questions[questionNumber].correct;
 
     check(choice);
-    questionNumber++;
-
-    // Set the correct number for the current question
-    $("#q-num").text(questionNumber+1);
+    if (questionNumber >= questions.length - 1) {
+        gameOver();
+    } else {
+        
+        // Wait 2 Seconds Before Moving to Next Question
+        setTimeout(function () {
+            
+            $(".rightWrong").text("");
+            questionNumber++;
     
-    // Set the Question and Answer Options
-    $("#q-text").text(questions[questionNumber].question);
-    $(".a1").text(questions[questionNumber].answer1);
-    $(".a2").text(questions[questionNumber].answer2);
-    $(".a3").text(questions[questionNumber].answer3);
-    $(".a4").text(questions[questionNumber].answer4);
+            // Set the correct number for the current question
+            $("#q-num").text(questionNumber+1);
+            
+            // Set the Question and Answer Options
+            $("#q-text").text(questions[questionNumber].question);
+            $(".a1").text(questions[questionNumber].answer1);
+            $(".a2").text(questions[questionNumber].answer2);
+            $(".a3").text(questions[questionNumber].answer3);
+            $(".a4").text(questions[questionNumber].answer4);
 
+        }, 2000);
+    }
 });
 
-currentScore = 0;
+var currentScore = 0;
 
 function check(a) {
     if (a != $ans) {
-        return $(".rightWrong").text('INCORRECT');
+        time -= 10;
+        timerEl.text(time);
+        return $(".rightWrong").text('INCORRECT').removeClass("green").addClass("red");
       } else { 
-        currentScore+ (1* $time);
-        return $(".rightWrong").text('CORRECT'); 
+        currentScore += time;
+        return $(".rightWrong").text('CORRECT').removeClass("red").addClass("green"); 
       }
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// $("label.btn").on('click',function () {
-//     var choice = $(this).find('input:radio').val();
-//     $ans = questions[questionNumber].correct;
-//         setTimeout(function(){
-//             $("#rightWrong").html($(this).checking(choice));     
-//             }, 500);
-    
-//     // console.log("Before adding 1: " + questionNumber);
-//     // questionNumber++;
-//     // console.log("After adding 1: " + questionNumber);
-    
-//     function check(choice) {
-//     if (choice != $ans)
-//         return $(".rightWrong").text('INCORRECT');
-//     else 
-//         return $(".rightWrong").text('CORRECT'); 
-//     }
-
-//     $.fn.checking = function(ck) { 
-//         if (ck != $ans)
-//             return $(".rightWrong").text('INCORRECT');
-//         else 
-//             return $(".rightWrong").text('CORRECT');
-//     }; 
-
-
-//     // Set the correct number for the current question
-//     $("#q-num").text(questionNumber+1);
-    
-//     // Set the Question and Answer Options
-//     $("#q-text").text(questions[questionNumber].question);
-//     $(".a1").text(questions[questionNumber].answer1);
-//     $(".a2").text(questions[questionNumber].answer2);
-//     $(".a3").text(questions[questionNumber].answer3);
-//     $(".a4").text(questions[questionNumber].answer4);
-// });
-
